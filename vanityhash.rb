f16ed59c8f3303d19d5d79e432b9b77d925eafaf
Ryan Finnie <ryan@finnie.org>
2016-11-10 12:16:57 -0800

    Add vanityhash

---
+++ vanityhash.rb
@@ -0,0 +1,11 @@
+class Vanityhash < Formula
+  desc "Brute forcing hashes for fun and profit"
+  homepage "https://github.com/rfinnie/vanityhash"
+  head "https://github.com/rfinnie/vanityhash.git"
+
+  def install
+    system "make", "all", "vanityhash.1"
+    bin.install "vanityhash"
+    man1.install "vanityhash.1"
+  end
+end
