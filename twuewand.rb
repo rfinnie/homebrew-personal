687bc289e15e1f2747fc21d756776c0c50f3608d
Ryan Finnie <ryan@finnie.org>
2020-06-17 19:22:15 -0700

    Update twuewand

--- twuewand.rb
+++ twuewand.rb
@@ -1,11 +1,14 @@
 class Twuewand < Formula
   desc "A truerand algorithm for generating entropy"
   homepage "https://github.com/rfinnie/twuewand"
-  head "https://github.com/rfinnie/twuewand.git"
+  head "https://github.com/rfinnie/twuewand.git", :branch => "main"
+
+  depends_on "python@3"
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    pyver = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{pyver}/site-packages"
+    system "python3", *Language::Python.setup_install_args(libexec)
     man1.install "doc/twuewand.1"
     bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
